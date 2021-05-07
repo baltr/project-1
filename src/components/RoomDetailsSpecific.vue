1a37e4feb36ad511642d9de70ad7266dc1baaf5a
Marko Rukavina <markorukavina4@gmail.com>
2021-05-07 13:10:28 +0200

    Added transition for hiddden elements, fixed link styling

--- src/components/RoomDetailsSpecific.vue
+++ src/components/RoomDetailsSpecific.vue
@@ -13,22 +13,24 @@
         <label for="summ">SUMMARY</label>
         <div id="summ"><span v-html="details.summary" /></div>
       </div>
-      <div v-if="isVisible">
-        <div class="col-5 fl-left specDetail">
-          <label for="org">ORGANIZER</label>
-          <div id="org">{{ details.organizer }}</div>
-        </div>
-        <div v-if="isVisible" class="col-5 fl-right specDetail ta-right">
-          <label for="no">ATTENDEES</label>
-          <div id="no">{{ details.attendees }}</div>
-        </div>
-        <div v-if="isVisible" class="col-12 fl-left specDetail">
-          <label for="desc">DESCRIPTION</label>
-          <div id="desc" class="description">
-            <span v-html="details.description" />
+      <transition name="collapse">
+        <div v-if="isVisible">
+          <div class="col-5 fl-left specDetail">
+            <label for="org">ORGANIZER</label>
+            <div id="org">{{ details.organizer }}</div>
+          </div>
+          <div v-if="isVisible" class="col-5 fl-right specDetail ta-right">
+            <label for="no">ATTENDEES</label>
+            <div id="no">{{ details.attendees }}</div>
+          </div>
+          <div v-if="isVisible" class="col-12 fl-left specDetail">
+            <label for="desc">DESCRIPTION</label>
+            <div id="desc" class="description">
+              <span v-html="details.description" />
+            </div>
           </div>
         </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -81,7 +83,7 @@ label {
 }
 .details a {
   text-decoration: revert;
-  color: revert;
+  color: #673ab7;
 }
 .specDetail {
   margin-bottom: 10px;
@@ -92,5 +94,16 @@ label {
 .description {
   padding-right: 20px;
   text-align: justify;
+}
+.collapse-enter-active,
+.collapse-leave-active {
+  transition: all 0.2s;
+  max-height: 230px;
+}
+.collapse-enter,
+.collapse-leave-to
+{
+  opacity: 0;
+  max-height: 0px;
 }
 </style>
