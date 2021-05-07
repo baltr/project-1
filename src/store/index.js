69274fcbba2918645a249f36c05f9265bdd4cdb9
Baltr <markorukavina4@gmail.com>
2021-05-07 21:32:33 +0200

    Properly implemented caching using store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -7,10 +8,26 @@ export default new Vuex.Store({
   state: {
     rooms: null,
   },
+  getters: {
+    getRooms: state => {
+      return state.rooms
+    }
+  },
   mutations: {
+    setRooms (state, payload){
+      state.rooms = payload
+    }
   },
   actions: {
-  },
-  modules: {
+    fetchRooms({context}){
+      axios
+      .get("https://meeting-rooms.superology.dev/", { headers: { authentication: "drSsLeYSzdWVgwqKFk6mFt66X3ZWETQW" } })
+      .then((response) => {
+        this.commit('setRooms', response.data.rooms)
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <h1>Superology Rooms</h1>
-    <div v-if="error">
-      An error has occurred in fetching data from the server.
-    </div>
-    <div v-else-if="rooms" class="home">
+    <div v-if="rooms" class="home">
       <div v-if="rooms.length">
         <Room v-for="room in rooms" :room="room" :key="room.id" />
       </div>
@@ -14,38 +11,16 @@
   </div>
 </template>
 
-<!-- IMPLEMENT STORE PROPERLY -->
-
 <script>
 import Room from "@/components/Room.vue";
-import axios from "axios";
 
 export default {
-  data() {
-    return {
-      rooms: null,
-      error: false,
-    };
-  },
-  methods:{
-    axiosGet(){
-      axios
-      .get("https://meeting-rooms.superology.dev/", { headers: { authentication: "drSsLeYSzdWVgwqKFk6mFt66X3ZWETQW" } })
-      .then((response) => {
-        this.rooms = response.data.rooms;
-        this.$store.rooms = this.rooms;
-      })
-      .catch((response) => {
-        console.log(response);
-        this.error = true;
-      });
-    }
-  },
   created() {
-    if (this.$store.rooms == null){
-      this.axiosGet();
-    } else {
-      this.rooms = this.$store.rooms
+    if (this.$store.getters.getRooms === null) this.$store.dispatch('fetchRooms');
+  },
+  computed:{
+    rooms(){
+      return this.$store.getters.getRooms
     }
   },
   name: "Home",
