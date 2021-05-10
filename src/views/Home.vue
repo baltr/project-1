bd72fa2c2306270145f6227facf298120de8f796
Marko Rukavina <markorukavina4@gmail.com>
2021-05-10 10:42:20 +0200

    Added search for list of rooms

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <h1>Superology Rooms</h1>
+    <input type="text" v-model="searchQuery" placeholder="Search Rooms"/>
     <div v-if="rooms" class="home">
       <div v-if="rooms.length">
-        <Room v-for="room in rooms" :room="room" :key="room.id" />
+        <Room v-for="room in filteredRooms" :room="room" :key="room.id" />
       </div>
       <div v-else-if="rooms.length == 0">No rooms have been found.</div>
     </div>
@@ -15,12 +16,26 @@
 import Room from "@/components/Room.vue";
 
 export default {
+  data(){
+    return{
+      searchQuery: '',
+    }
+  },
   created() {
     if (this.$store.getters.getRooms === null) this.$store.dispatch('fetchRooms');
   },
   computed:{
     rooms(){
       return this.$store.getters.getRooms
+    },
+    filteredRooms(){
+      if(this.searchQuery.length >= 3){
+        return this.rooms.filter((room) => {
+          return room.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+        })
+      }else{
+        return this.rooms;
+      }
     }
   },
   name: "Home",
